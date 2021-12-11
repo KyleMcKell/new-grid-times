59dca5229522ab2a57a78e36a78cfaf70942c9db
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-12-11 12:13:49 -0700

    adjusted footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Twitter, Facebook } from 'react-feather';
 import styled from 'styled-components/macro';
+import { QUERIES } from '../../constants';
 import MaxWidthWrapper from '../MaxWidthWrapper';
 
 import VisuallyHidden from '../VisuallyHidden';
@@ -140,6 +141,16 @@ const TopRow = styled.div`
 	font-size: 0.875rem;
 	border-bottom: 1px solid var(--color-gray-700);
 	padding: 24px 0;
+
+	@media ${QUERIES.tabletAndUp} {
+		flex-direction: row;
+		justify-content: center;
+		gap: 48px;
+	}
+
+	@media ${QUERIES.laptopAndUp} {
+		justify-content: flex-end;
+	}
 `;
 
 const Social = styled.div`
@@ -166,6 +177,13 @@ const MainNavArea = styled.div`
 	gap: 32px;
 	padding: 32px 0 48px;
 	text-align: center;
+
+	@media ${QUERIES.tabletAndUp} {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(175px, 1fr));
+		text-align: left;
+		flex-direction: revert;
+	}
 `;
 
 const MainNavHeading = styled.h2`
@@ -192,6 +210,10 @@ const Subfooter = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
+
+	@media ${QUERIES.laptopAndUp} {
+		align-items: flex-start;
+	}
 `;
 
 const Logo = styled.a`
